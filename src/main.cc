c610e58891af9a4fc2a701af12e61caeebd2ffe0
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2013-05-08 21:51:22 +0200

    Font changes

--- src/main.cc
+++ src/main.cc
@@ -11,8 +11,7 @@ int main(int argc, char *argv[])
 
     // Lägg till de font som används.
     QFontDatabase::addApplicationFont(":/fonts/resources/consolas.ttf");
-    QFontDatabase::addApplicationFont(":/fonts/resources/ubuntu.ttf");
-    QFontDatabase::addApplicationFont(":/fonts/resources/ptsans.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/resources/opensans.ttf");
     QApplication::setStyle(QStyleFactory::create("Fusion"));
 
     MC::MainWindow w;
